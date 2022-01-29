f365cdab5e98cfddecbb35da2bbeb335d44dc1c6
rendiwithi <rendidwih2@gmail.com>
2022-01-29 22:39:12 +0700

    fix update for artikel(rendiwithi)

--- routers/artikel.js
+++ routers/artikel.js
@@ -5,8 +5,8 @@ const image = require("../middleware/storage");
 
 router.get("/", controller.artikel.getAll);
 router.get("/:id", controller.artikel.getById);
-router.post("/", image.single("foto"), controller.artikel.createNew);
-router.put("/", controller.artikel.editAt);
+router.post("/", image.single("image"), controller.artikel.createNew);
+router.put("/", image.single("image"), controller.artikel.editAt);
 router.delete("/", controller.artikel.deleteAt);
 
 module.exports = router;
